8de58f353777fa3a4a2c1b4a4da48c65ae131aad
IR101D <somefkingadress@gmail.com>
2025-09-17 23:04:21 +0100

    Update Button.tsx

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { Buttonprops } from "@/interfaces";
 
-const Button: React.FC<Buttonprops> = ({title, size="medium",shape="rounded-md"}) => {
-    const sizeClasses = {
-          small: "px-3 py-1 text-sm",
-          medium: "px-5 py-2 text-base",
-          large: "px-7 py-3 text-lg",
-    }
+const Button: React.FC<Buttonprops> = ({title, styles}) => {
     return (
-        <button className={"bg-blue-600 text-white font-semibold $(sizeClasses[size]} ${shape}"}>
+        <button className={"bg-blue-600 text-white font-semibold ${syles}"}>
             {title}
         </button>
     )
